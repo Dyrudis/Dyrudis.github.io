aeed1df93be5c20040b83e7a2e427bdb378ee7ab
Dyrudis <58176532+Dyrudis@users.noreply.github.com>
2022-02-13 23:50:26 +0100

    Update

--- isaac/game.js
+++ isaac/game.js
@@ -1,4 +1,38 @@
+let life = 12;
+let maxlife = life % 2 ? life + 1 : life;
+let score = 0;
+
+function showLife(life) {
+    if (life > maxlife) {
+        life = maxlife;
+    }
+    if (life < 0) {
+        life = 0;
+    }
+    document.getElementById("life").innerText = "";
+    for (let i = 0; i < Math.floor(life / 2); i++) {
+        document.getElementById("life").innerHTML += "<img src='resources/heart1.png' alt='heart'/>";
+    }
+    if (life % 2 == 1) {
+        document.getElementById("life").innerHTML += "<img src='resources/heart2.png' alt='heart'/>";
+        for (i = Math.floor(life / 2) + 1; i < Math.floor(maxlife / 2); i++) {
+            document.getElementById("life").innerHTML += "<img src='resources/heart3.png' alt='heart'/>";
+        }
+    } else {
+        for (i = Math.floor(life / 2) + 1; i <= Math.floor(maxlife / 2); i++) {
+            document.getElementById("life").innerHTML += "<img src='resources/heart3.png' alt='heart'/>";
+        }
+    }
+}
+
+function showScore(score) {
+    document.getElementById("score").innerText = score;
+}
+
 function play() {
+    let audio = new Audio("resources/next.wav");
+    audio.volume = 0.15;
+    audio.play();
     document.getElementById("next").style.display = "none";
     // Select four random items
     let selectedItems = [];
@@ -19,25 +53,61 @@ function play() {
     }
 
     let goodAnswer = Math.floor(Math.random() * 4);
-
     document.getElementById("items").innerHTML = "";
     selectedItems.forEach((item, index) => {
+        let div = document.createElement("div");
         let p = document.createElement("p");
         p.innerText = item.name;
-        p.addEventListener("click", () => {
+        div.addEventListener("click", click);
+
+        function click() {
             if (index == goodAnswer) {
-                p.style.backgroundColor = "#2bad24";
+                div.classList.add("good");
                 document.getElementById("next").style.display = "block";
+                let audio = new Audio("resources/thumbs up.wav");
+                audio.volume = 0.07;
+                audio.play();
+                score+= 100;
+                showScore(score);
             } else {
-                p.style.backgroundColor = "#eb4034";
+                div.classList.add("bad");
+                let audio = new Audio("resources/hurt" + (Math.round(Math.random() * 2) + 1) + ".wav");
+                audio.volume = 0.1;
+                audio.play();
+                showLife(--life);
+                score -= 10;
+                showScore(score);
+            }
+            div.style.cursor = "default";
+            div.removeEventListener("click", click);
+        }
+
+        let img = document.createElement("img");
+        let src = "resources/items/" + item.id.padStart(3, "0") + ".png";
+        fetch(src).then((res) => {
+            if (res.ok) {
+                img.src = src;
+            } else {
+                img.src = "resources/items/questionmark.png";
             }
         });
-        document.getElementById("items").appendChild(p);
+
+        div.appendChild(img);
+        div.appendChild(p);
+
+        document.getElementById("items").appendChild(div);
+
         if (index == goodAnswer) {
-            document.getElementById("description").innerText = item.desc.replaceAll("#", "\n");
+            let desc = item.desc.replaceAll("#", "</br>");
+            desc = desc.replaceAll("↑", "<img src='resources/up.png'></img>");
+            desc = desc.replaceAll("↓", "<img src='resources/down.png'></img>");
+            desc = desc.replaceAll("!!!", "<img src='resources/warning.png'></img>");
+            document.getElementById("description").innerHTML = desc;
         }
     });
 }
 
 document.getElementById("next").addEventListener("click", play);
+showLife(life);
+showScore(score);
 play();
